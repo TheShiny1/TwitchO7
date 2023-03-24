300ff64f1c99424dc7365b50fda9d5611cf3371b
TheShiny1 <shinomiyaa@allegheny.edu>
2023-03-24 12:21:27 -0400

    update prdiction.py

--- Prediction.py
+++ Prediction.py
@@ -1,13 +1,10 @@
 import os
 import pandas as pd
 
-# set the directory path
 dir_path = input("Input path to directory that you want to analyze: ")
 
-# read all csv files in the directory
 csv_files = [f for f in os.listdir(dir_path) if f.endswith('.csv')]
 
-# create an empty list to store the combined data from all files
 combined_data = []
 
 # loop through the csv files and append their data to the combined_data list
@@ -16,35 +13,30 @@ for file in csv_files:
     data = pd.read_csv(file_path)
     combined_data.append(data)
 
-# combine all the data from the csv files
 all_data = pd.concat(combined_data)
 
-# calculate the average viewer count of all the csv files combined
 average_viewer_count = all_data["Viewer Count"].mean()
 
-# print out the average viewer count
-print("The average viewer count of all the csv files combined is:", average_viewer_count)
+timestamp_list = []
 
-# find the timestamps where the viewer count is higher than the average in all_data
-above_average_data = all_data[all_data["Viewer Count"] > average_viewer_count]
+# loop through the csv files again and find the timestamps where the viewer count is higher than the average
+for file in csv_files:
+    file_path = os.path.join(dir_path, file)
+    data = pd.read_csv(file_path)
+    above_average_data = data[data["Viewer Count"] > average_viewer_count]
+    if not above_average_data.empty:
+        above_average_data.reset_index(drop=True, inplace=True)
+        start_timestamp = above_average_data.iloc[0]["Timestamp"]
+        end_timestamp = above_average_data.iloc[-1]["Timestamp"]
+        timestamp_list.append([start_timestamp, end_timestamp])
 
-if not above_average_data.empty:
-    print("Timestamps where viewer count is higher than the average:")
-    
-    # find the start and end timestamp where the viewer count is higher than the average
-    above_average_data.reset_index(drop=True, inplace=True)
-    end_timestamp = above_average_data.iloc[0]["Timestamp"]
-    start_timestamp = above_average_data.iloc[-1]["Timestamp"]
-    print("Start timestamp:", start_timestamp)
-    print("End timestamp:", end_timestamp)
-    
-    # find the pattern of the start and end timestamp where the viewer count is higher than the average
-    pattern_start = start_timestamp
-    pattern_end = end_timestamp
-    for i in range(1, len(above_average_data)):
-        if above_average_data.iloc[i]["Timestamp"] != above_average_data.iloc[i-1]["Timestamp"] + pd.Timedelta(minutes=2):
-            pattern_end = above_average_data.iloc[i-1]["Timestamp"]
-            pattern_start = above_average_data.iloc[i]["Timestamp"]
-    pattern_end = end_timestamp
+# calculate the mean of the start and end timestamps where the viewer count is higher than the average
+if timestamp_list:
+    timestamp_list = pd.DataFrame(timestamp_list, columns=["Start Timestamp", "End Timestamp"])
+    mean_start_timestamp = timestamp_list["Start Timestamp"].apply(pd.to_timedelta).mean()
+    mean_end_timestamp = timestamp_list["End Timestamp"].apply(pd.to_timedelta).mean()
+
+    print("Mean start timestamp:", str(mean_start_timestamp))
+    print("Mean end timestamp:", str(mean_end_timestamp))
 else:
-    print("There is no data where viewer count is higher than the average.")
+    print("No timestamps found where viewer count is higher than the average.")
