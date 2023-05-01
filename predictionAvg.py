d6af062418c556c9a84092e733cdb77494b6d3f1
TheShiny1 <shinomiyaa@allegheny.edu>
2023-04-30 20:31:49 -0400

    add predictionAvg.py

---
+++ predictionAvg.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+path = 'data/moistcr1tikal/Wednesday'
+
+averages = []
+
+for filename in os.listdir(path):
+    if filename.endswith('.csv'):
+        df = pd.read_csv(os.path.join(path, filename))
+        avg = df['Viewer Count'].mean()
+        averages.append(avg)
+
+X = [[i] for i in range(len(averages))]
+y = averages
+
+# Train the linear regression model
+reg = LinearRegression().fit(X, y)
+
+next_avg = reg.predict([[len(averages)]])[0]
+
+print(f"The predicted average of the next CSV file is: {next_avg}")
